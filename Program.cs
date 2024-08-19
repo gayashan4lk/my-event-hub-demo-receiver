015c53cfa40b3108118b8b994b3a4520134aa847
gayashan4lk <gayashan4lk@gmail.com>
2024-08-19 15:18:14 +0530

    receive events

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Messaging.EventHubs.Consumer;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -8,5 +10,15 @@ var configuration = new ConfigurationBuilder()
 var EVENT_HUB_CONNECTION_STRING = configuration["AppSettings:EVENT_HUB_CONNECTION_STRING"];
 var EVENT_HUB_INSTANCE_NAME = configuration["AppSettings:EVENT_HUB_INSTANCE_NAME"];
 
-Console.WriteLine(EVENT_HUB_CONNECTION_STRING);
-Console.WriteLine(EVENT_HUB_INSTANCE_NAME);
+await using (var consumerClient = new EventHubConsumerClient(
+    EventHubConsumerClient.DefaultConsumerGroupName, 
+    EVENT_HUB_CONNECTION_STRING, 
+    EVENT_HUB_INSTANCE_NAME))
+{
+    Console.WriteLine("Listening for events...");
+    await foreach (PartitionEvent partitionEvent in consumerClient.ReadEventsAsync())
+    {
+        string data = Encoding.UTF8.GetString(partitionEvent.Data.EventBody.ToArray());
+        Console.WriteLine($"Message received. Partition: '{partitionEvent.Partition.PartitionId}', Data: '{data}'");
+    }
+}
